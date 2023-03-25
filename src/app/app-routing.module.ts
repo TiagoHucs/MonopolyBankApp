2693ae59b2eac3a9719746f87ccc3476a8f4a096
Tiago S. Hucs <tiagohucs@gmail.com>
2023-03-25 16:12:14 +0000

    conceito

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login' , pathMatch: 'prefix' },
+  {path: 'login', component: LoginComponent },
+  {path: 'layout', component: LayoutComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }

c663a7ee6d065748169c6c48d5f7ce5433f2c666
Tiago <tiagohucs@gmail.com>
2023-04-08 16:17:43 -0300

    organizando projeto

---
+++ server/src/main/java/com/MonopolyBankAppServer/MonopolyBankAppServerApplication.java
@@ -0,0 +1,13 @@
+package com.MonopolyBankAppServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonopolyBankAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonopolyBankAppServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/MonopolyBankAppServer/MonopolyBankAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MonopolyBankAppServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonopolyBankAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
